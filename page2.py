56f70991850003e5e2278404467d5c798473f591
Bianca A. Hernandez <bhernan2@gmail.com>
2020-07-13 07:40:34 -0500

    updated and running on server

--- page2.py
+++ page2.py
@@ -3,16 +3,22 @@ import dash_bootstrap_components as dbc
 import dash_core_components as dcc
 import dash_html_components as html
 
-from navbar import dash_navbar
-from tally_cards import tallies, title_authors
-
-nav = dash_navbar()
-titleauthors = title_authors()
-tallycards = tallies()
-
 #global confirmed, recovered and deaths row
-container = dbc.Card([dbc.CardBody([dbc.Container([ 
-            html.H1(children='This is where we can link tableau, maybe with an image of visualization?', className='mt-5 py-4 pb-3 text-center')
+container1 = dbc.Card([dbc.CardBody([dbc.Container([ 
+            html.H1(children='This is where we can link tableau, or create a plotly visualization?', className='mt-5 py-4 pb-3 text-center')
             ])])])
+container2 = dbc.Card([dbc.CardBody([dbc.Container([ 
+            html.H1(children='This is where we can put an ML simulator', className='mt-5 py-4 pb-3 text-center')
+            ])])])
+def container_box():
+    value = container1
+    return value
+def container_box2():
+    value = container2
+    return value
+
+
+
+
 
 
