6b629a5c8f92dad382775577de1fbd4ebcb20a17
Bianca A. Hernandez <bhernan2@gmail.com>
2020-07-12 20:23:46 -0500

    added homepage.py file

---
+++ homepage.py
@@ -0,0 +1,29 @@
+import dash
+import dash_bootstrap_components as dbc
+import dash_core_components as dcc
+import dash_html_components as html
+
+from navbar import dash_navbar
+from page1 import tallies, title_authors, bubble_animation_col, global_line_graph, footer
+
+
+nav = dash_navbar()
+titleauthors = title_authors()
+tallycards = tallies()
+bubble_animation_col = bubble_animation_col()
+global_line_graph = global_line_graph()
+footer = footer()
+def Homepage():
+    layout = html.Div([
+    nav,
+    titleauthors,
+    tallycards, 
+    bubble_animation_col, 
+    global_line_graph,
+    footer
+    ])
+    return layout 
+
+
+
+
